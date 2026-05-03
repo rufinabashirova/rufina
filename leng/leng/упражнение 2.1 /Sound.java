926b9453f6d33baa9a6c986b8cba4a4e994c87d8
rufinabashirova <henda.form@mail.ru>
2026-05-03 20:26:24 +0300

    Delete leng/leng directory

--- leng/leng/упражнение 2.1 /Sound.java
+++
@@ -1,8 +0,0 @@
-class Sound {
-    public static void main(String[] args) {
-        double dist;
-        dist = 7.2 * 335;
-        System.out.println("Место вспышки молнии находится на расстоянии "
-                + dist + " метров.");
-    }
-}
